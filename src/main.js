80c9db7d8baaa669c6e8b77b40b8f10030d440f8
mmilano <mmilano+git@gmail.com>
2020-08-13 11:06:26 -0700

    beginning to customize.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
 Vue.config.productionTip = false
---
+++ vue.config.js
@@ -0,0 +1,45 @@
+
+
+module.exports = {
+    outputDir: "build",
+    assetsDir: "assets",
+    runtimeCompiler: true,
+    lintOnSave: false,
+
+    // modify the index.html's <title>
+    chainWebpack: (config) => {
+        config.plugin("html").tap((args) => {
+            args[0].title = "ThoriumData";
+            return args;
+        });
+//         config.plugin("fork-ts-checker").tap((args) => {
+//             args[0].memoryLimit = 2096;
+//             return args;
+//         });
+
+        // clear out the less rules in the hope that this will speed things up a tiny tiny bit
+        const lessRules = config.module.rule("less");
+        lessRules.uses.clear();
+        lessRules.store.clear();
+        lessRules.oneOfs.store.clear();
+
+        // clear out the STYLUS rules in the hope that this will speed things up a tiny tiny bit
+        const stylusRules = config.module.rule("stylus");
+        stylusRules.uses.clear();
+        stylusRules.store.clear();
+        stylusRules.oneOfs.store.clear();
+
+        // clear out the media rules in the hope that this will speed things up a tiny tiny bit
+        const mediaRules = config.module.rule("media");
+        mediaRules.uses.clear();
+        mediaRules.store.clear();
+        mediaRules.oneOfs.store.clear();
+    },
+
+
+    pluginOptions: {
+    },
+    css: {
+        sourceMap: true,
+    },
+};
