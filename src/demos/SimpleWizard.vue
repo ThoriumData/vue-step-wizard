cfb0c73664e0a25133453a984455946590800770
mmilano <mmilano+git@gmail.com>
2020-08-17 11:31:56 -0700

    beginning to try to integrate a modal component

--- src/demos/SimpleWizard.vue
+++ src/demos/SimpleWizard.vue
@@ -30,27 +30,33 @@
         </tab-content>
 
         <tab-content title="Connection Details">
+
+            <h3>
+                Select the type of data source.
+            </h3>
+
             <div class="form-group">
-                <label for="companyName">Your Company Name</label>
-                <input type="text" class="form-control" :class="hasError('companyName') ? 'is-invalid' : ''" placeholder="Enter your Company / Organization name" v-model="formData.companyName">
-                <div v-if="hasError('companyName')" class="invalid-feedback">
-                    <div class="error" v-if="!$v.formData.companyName.required">Please provide a valid company name.</div>
+                <label for="fullName">Full Name</label>
+                <input type="text" class="form-control" :class="hasError('fullName') ? 'is-invalid' : ''" placeholder="Enter your name" v-model="formData.fullName">
+                <div v-if="hasError('fullName')" class="invalid-feedback">
+                    <div class="error" v-if="!$v.formData.fullName.required">Please provide a valid name.</div>
                 </div>
             </div>
             <div class="form-group">
-                <label for="numberOfEmployees">Number of Employees</label>
-                <input type="text" class="form-control" :class="hasError('numberOfEmployees') ? 'is-invalid' : ''" placeholder="Enter Total Number of Employees" v-model="formData.numberOfEmployees">
-                <div v-if="hasError('numberOfEmployees')" class="invalid-feedback">
-                    <div class="error" v-if="!$v.formData.numberOfEmployees.required">Please provide number of employees in your company.</div>
-                    <div class="error" v-if="!$v.formData.numberOfEmployees.numeric">Should be a valid value.</div>
+                <label for="email">Your Email</label>
+                <input type="email" class="form-control" :class="hasError('email') ? 'is-invalid' : ''" placeholder="Enter your email" v-model="formData.email">
+                <div v-if="hasError('email')" class="invalid-feedback">
+                    <div class="error" v-if="!$v.formData.email.required">Email field is required</div>
+                    <div class="error" v-if="!$v.formData.email.email">Should be in email format</div>
                 </div>
             </div>
+
         </tab-content>
 
         <tab-content title="Select Data Sets">
-            <h2>
-
-            </h2>
+            <h3>
+                Select the individual data sets.
+            </h3>
             <div class="form-group">
                 <label for="referral">From Where did you hear about us</label>
                 <select :class="hasError('referral') ? 'is-invalid' : ''" class="form-control" v-model="formData.referral">
@@ -60,12 +66,12 @@
                     <option>Other</option>
                 </select>
                 <div v-if="hasError('referral')" class="invalid-feedback">
-                    <div class="error" v-if="!$v.formData.referral.required">Please select on of the fields.</div>
+                    <div class="error" v-if="!$v.formData.referral.required">Please select one of the fields.</div>
                 </div>
             </div>
             <div class="form-group form-check">
                 <input type="checkbox" :class="hasError('terms') ? 'is-invalid' : ''" class="form-check-input" v-model="formData.terms">
-                <label class="form-check-label">I accpet terms & conditions</label>
+                <label class="form-check-label">I accept terms and conditions</label>
                 <div v-if="hasError('terms')" class="invalid-feedback">
                     <div class="error" v-if="!$v.formData.terms.required">Please select terms and conditions.</div>
                 </div>
@@ -73,9 +79,9 @@
         </tab-content>
 
         <tab-content title="Verify">
-            <h2>
-
-            </h2>
+            <h3>
+                Provide individual details for each selected asset.
+            </h3>
             <div class="form-group">
                 <label for="referral">From Where did you hear about us</label>
                 <select :class="hasError('referral') ? 'is-invalid' : ''" class="form-control" v-model="formData.referral">
@@ -85,12 +91,12 @@
                     <option>Other</option>
                 </select>
                 <div v-if="hasError('referral')" class="invalid-feedback">
-                    <div class="error" v-if="!$v.formData.referral.required">Please select on of the fields.</div>
+                    <div class="error" v-if="!$v.formData.referral.required">Please select one of the fields.</div>
                 </div>
             </div>
             <div class="form-group form-check">
                 <input type="checkbox" :class="hasError('terms') ? 'is-invalid' : ''" class="form-check-input" v-model="formData.terms">
-                <label class="form-check-label">I accpet terms & conditions</label>
+                <label class="form-check-label">I accept terms and conditions</label>
                 <div v-if="hasError('terms')" class="invalid-feedback">
                     <div class="error" v-if="!$v.formData.terms.required">Please select terms and conditions.</div>
                 </div>
@@ -98,7 +104,11 @@
         </tab-content>
     </form-wizard>
 
+    <modal>
 
+
+
+    </modal>
     </div>
 
 
@@ -109,6 +119,8 @@
 <script>
 import FormWizard from '../components/FormWizard.vue';
 import TabContent from '../components/TabContent.vue';
+import Modal from '../components/Modal.vue';
+
 import ValidationHelper from '../components/ValidationHelper.vue';
 
 import { required, email, numeric } from 'vuelidate/lib/validators';
@@ -118,7 +130,9 @@ const checked = (value) => value === true;
 export default {
     name: 'SimpleWizard',
     components: {
-        FormWizard, TabContent
+        FormWizard,
+        TabContent,
+        Modal
     },
     mixins: [ValidationHelper],
     data(){
@@ -154,6 +168,7 @@ export default {
         },
 
         reset() {
+            console.log("[simplewizard] resetting the form.");
             for(let field in this.formData){
                 this.formData[field] = null;
             }
